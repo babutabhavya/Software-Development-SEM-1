8ef7b8bcb02902adb3874425830da5daa9edbd08
Bhavya Babuta <bhavya.babuta@veygo.com>
2024-01-17 21:45:06 +0100

    Add testing for the book views and configure pytest

--- src/models/response.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class ResponseModel(BaseModel):
-    message: str
-    status_code: int
--- src/views/health.py
+++ src/views/health.py
@@ -1,10 +1,8 @@
-from flask import Blueprint
-
-from models.response import ResponseModel
+from flask import Blueprint, Response, make_response
 
 health_blueprint = Blueprint("health", __name__, url_prefix="/health")
 
 
 @health_blueprint.route("/", methods=["GET"])
 def health():
-    return ResponseModel(message="Success", status_code=200).model_dump()
+    return Response(make_response({"message": "Success"}), status=200)
