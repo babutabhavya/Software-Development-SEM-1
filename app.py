6e4e04eac41e9d6224ed6079aecf8cbdb437e343
Bhavya Babuta <bhavya.babuta@veygo.com>
2024-01-17 19:04:51 +0100

    Add .gitignore, makefile to run and setup the flask application, folder structure and a health endpoint

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-from flask import Flask, jsonify, request
-
-app = Flask(__name)
-
-@app.route('/')
-def hello_world():
-    response_data = {"message": "Hello, World!"}
-    return jsonify(response_data)
-
-@app.route('/sum', methods=['POST'])
-def sum_numbers():
-    data = request.get_json()
-
-    if 'num1' in data and 'num2' in data:
-        result = data['num1'] +  data['num2']
-        return jsonify({"result": result})
-    else:
-        return jsonify({"error": "Please provide 'num1' and 'num2' in the request body."}, 400)
+from flask_sqlalchemy import SQLAlchemy
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask
+from src.views import health_blueprint
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite.db"
+db = SQLAlchemy(app)
+
+app.register_blueprint(health_blueprint)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
